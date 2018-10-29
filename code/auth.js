63df717e10f1d45a4fe4d622b3324461c744d507
shah-altamash92 <43642211+shah-altamash92@users.noreply.github.com>
2018-10-29 15:19:27 +0530

    Update auth.js

--- code/auth.js
+++ code/auth.js
@@ -1,192 +1,71 @@
 const OAuth = require('oauth-1.0a');
 var oauthSignature = require('oauth-signature');
 
- const CONSUMER_KEY = 'qj9dvvss3bogrryquze5c52alzxyjlnf'
- const CONSUMER_SECRET = 'j4uff38y4qg88j0edxyf7dep4dn095whsia9ms5e'
- 
-
- const oauth = OAuth({
+ const oauth = (props) => OAuth({
   
-  consumer: { key: CONSUMER_KEY, secret: CONSUMER_SECRET}, // React native Progress keys
+  consumer: { key:props.CONSUMER_KEY, secret: props.CONSUMER_SECRET}, // React native Progress keys
   
   signature_method: 'HMAC-SHA1',
   hash_function(base_string, key) {
-
+   
       var initialSeparator = base_string.indexOf('&');
       var basePath = decodeURIComponent( base_string.substring(initialSeparator+1, base_string.indexOf('&', 5)))
       var search = decodeURIComponent(base_string.substring(base_string.indexOf('&', 5)+1))
-
-      // console.log(basePath);
-      // console.log(search);
-
       var parameters = JSON.parse('{"' + (search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
       var tokenSecret = parameters.oauth_secret;
-      // console.log(tokenSecret);
-
+  
       if(basePath.indexOf('request-token') > 0)
           parameters.oauth_callback = 'x-com-frogtrade-frogprogress-oauth://success'
-      // console.log(parameters);
-
+          console.log("screat"+parameters.CONSUMER_SECRET);
       var encodedSignature;
+      var consumer_SECRET = parameters.CONSUMER_SECRET;
+      
       if(tokenSecret){     
         
           delete parameters.oauth_secret
           basePath = encodeURIComponent (basePath);
-          encodedSignature = oauthSignature.generate('POST', basePath, parameters, CONSUMER_SECRET, tokenSecret, { encodeSignature: false})
+          encodedSignature = oauthSignature.generate('POST', basePath, parameters, consumer_SECRET, tokenSecret, { encodeSignature: false})
       }
       else
-          encodedSignature = oauthSignature.generate('GET', basePath, parameters, CONSUMER_SECRET)
-
-          //console.log("un-Encoded Signature in get Request token:" + encodedSignature); 
-          //encodedSignature = decodeURIComponent(encodedSignature)
-          //console.log("encodedSignature:" + encodedSignature);
-         
+          encodedSignature = oauthSignature.generate('GET', basePath, parameters,consumer_SECRET)
 
       return encodedSignature;
   }
 })
 
-const oauth1 = OAuth({
+const oauth1 =  (props) => OAuth({
   
-    consumer: { key:  CONSUMER_KEY, secret: CONSUMER_SECRET}, // React native Progress keys
-     
+    consumer: { key:props.CONSUMER_KEY, secret:props.CONSUMER_SECRET}, // React native Progress keys
+    
     signature_method: 'HMAC-SHA1',
      hash_function(base_string, key) {
-    //    console.log(decodeURIComponent(base_string));
-   
-
+  
        var initialSeparator = base_string.indexOf('&');
        initialSeparator = base_string.indexOf('&');
        var methodType = base_string.substring(0, initialSeparator);
        var basePath = decodeURIComponent( base_string.substring(initialSeparator+1, base_string.indexOf('&', 5)))
-    //    console.log(basePath);
-
-    //    //React native keys
-    //    consumerKey = 'qj9dvvss3bogrryquze5c52alzxyjlnf'
-    //    consumerSecret = 'j4uff38y4qg88j0edxyf7dep4dn095whsia9ms5e'
-   
+    
        var search = decodeURIComponent(base_string.substring(base_string.indexOf('&', 5)+1))
-    //    console.log(search);
   
        var parameters = JSON.parse('{"' + (search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-    //    console.log(parameters);
 
        if(parameters.sections)
         parameters.sections = decodeURIComponent(parameters.sections);
   
        var tokenSecret = parameters.oauth_secret;
-  
        delete parameters.oauth_secret
-    //    console.log(parameters);
        var encodedURI = encodeURIComponent (basePath);
-      
-    //    console.log("basePath while generating signature: " + basePath);
-    //    console.log("encodedURI while generating signature: " + encodedURI);
-    //    console.log(parameters);
-    //    console.log("consumerSecret while generating signature: " + CONSUMER_SECRET);
-    //    console.log("tokenSecret while generating signature: " + tokenSecret);
-   
-       // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
-       //encodedSignature = oauthSignature.generate('GET', basePath, parameters, consumerSecret, tokenSecret);
-       var encodedSignature = oauthSignature.generate(methodType, basePath, parameters, CONSUMER_SECRET, tokenSecret,{ encodeSignature: false})
+
+       var consumer_SECRET = parameters.CONSUMER_SECRET;
+       delete parameters.CONSUMER_SECRET
+
+       var encodedSignature = oauthSignature.generate(methodType, basePath, parameters,consumer_SECRET, tokenSecret,{ encodeSignature: false})
   
-       //console.log("encodedSignature oauth1: " + encodedSignature);
        if(basePath.indexOf('access-token')>-1)
             encodedSignature = encodeURIComponent(encodedSignature)
-        //console.log("decodedSignature : " + encodedSignature);
-        
        return encodedSignature;
      }
    });
    
-   
-  
-   
-
-// const oauth2 = OAuth({
-  
-//     consumer: { key: 'qj9dvvss3bogrryquze5c52alzxyjlnf', secret: 'j4uff38y4qg88j0edxyf7dep4dn095whsia9ms5e'}, // React native Progress keys
-     
-//     signature_method: 'HMAC-SHA1',
-//      hash_function(base_string, key) {
-
-//        var initialSeparator = base_string.indexOf('&');
-//        var methodType = base_string.substring(0, initialSeparator);
-//        var basePath = decodeURIComponent( base_string.substring(initialSeparator+1, base_string.indexOf('&', 5)))
-
-//        consumerKey = 'qj9dvvss3bogrryquze5c52alzxyjlnf'
-//        consumerSecret = 'j4uff38y4qg88j0edxyf7dep4dn095whsia9ms5e'
-   
-//        var search = decodeURIComponent(base_string.substring(base_string.indexOf('&', 5)+1))
-
-//        var parameters = JSON.parse('{"' + (search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-//        var tokenSecret = parameters.oauth_secret;
-        
-//        parameters.sections = decodeURIComponent(parameters.sections);
-  
-//        delete parameters.oauth_secret
-//        var encodedURI = encodeURIComponent (basePath);
-//       var encodedSignature = oauthSignature.generate(methodType, basePath, parameters, consumerSecret, tokenSecret,
-//        { encodeSignature: false})
-//         encodedSignature = decodeURIComponent(encodedSignature)
-
-//        return encodedSignature;
-//      }
-//    });
-   
-// const getRequestToken = (schoolUrl) => {
-//     console.log('schoolUrl'+schoolUrl);
-//    _this = this;
-//    const request_data = {
-//        url: schoolUrl + '/api/2/oauth1.php/request-token',
-//        method: 'GET',
-//    };
-//    //console.log('schoolUrl'+schoolUrl);
-   
- 
-//    params = oauth.authorize(request_data);
-//    params.oauth_callback = 'x-com-frogtrade-frogprogress-oauth://success'
-//    var query = Object.keys(params)
-//        .map(k => k + '=' + params[k])
-//        .join('&');
-//    //  console.log (params)
-//    //  console.log(query);
- 
-//    var oauthPath = request_data.url + '?' + query
-
-   
- 
-//    fetch(oauthPath).then(function (response) {
-   
-//        if (response.status !== 200) {
-//            console.log('Looks like there was a problem. Status Code: ', response.status, response);
-//            _this.setState({
-//                requestToken: null
-//            })
-//            return;
-//        }
-//        else {
-//            response.text().then(function (text) {
- 
-//                var route = decodeURIComponent(text.substring(text.indexOf('?') + 1))
-//                // console.log('handleOpenURL url is: ' + route);
-//                var authParams = JSON.parse('{"' + (route).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
-//                console.log('token:--> ' + authParams['oauth_token_secret'])
- 
-//               // _this.hideLoading();
-//                _this.setState({
-//                    redirectUrl: schoolUrl + '/app/oauthconsent' + '?' + text,
-//                    loadWebView: true,
-//                    // requestToken: text,
-//                    oauth_token_secret: authParams['oauth_token_secret']
-//                });
- 
-//            });
-//        }
-      
-//        return response;
-//    })
-//  }
-
 
 export {oauth,oauth1};
